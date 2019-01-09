f9b556966e84b5ffdb180ac6d2861fd19f3128cb
Matt Weidner <matt.weidner@gmail.com>
2019-01-08 21:30:01 -0600

    Add email function to contact form.

---
+++ js/elasticemail.js
@@ -0,0 +1,20 @@
+function notifyUser(message) {
+	if (message == "OK") { 
+		alert("Message sent.");
+		window.location.href = "/contact.html";
+	} else { 
+		alert("Error sending message.\n"+message);
+	}
+}
+
+function sendMail() {
+	var fname = document.getElementById('fname').value;
+	var lname = document.getElementById('lname').value;
+	var email = document.getElementById('email').value;
+	var subject = document.getElementById('subject').value;
+	var message = document.getElementById('message').value;
+	if (fname == "" || lname == "" || email == "" || subject == "" || message == "") {
+		alert("Please complete the entire form. All elements are required.");
+		return;
+	}
+	Email.send({ SecureToken : "49c0bb81-2d92-4dd4-a2b5-df477fe315e0", To : 'offtherailsconnect@gmail.com', From : "offtherailswebmailer@gmail.com", Subject : subject, Body : fname+" "+lname+" <"+email+">\n\n"+message+"\n" }).then( message => notifyUser(message)); }
